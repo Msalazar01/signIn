7aed800dfb070b4cc4d4ed3754ddef27c2d4ae42
Marcela Salazar <Marcela@marcela-salazars-macbook.local>
2015-09-25 17:50:39 -0400

    inClass activity

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.signin')
+	.on('click', function(e){
+		$('.modal').fadeIn('1000');
+	});
+
+$('.close')
+	.on('click', function(e){
+		$('.modal').fadeOut('1000');
+	});
+
+$('.submit')
+	.on('click', function(e){
+		$('input').addClass('error');
+	});
+
+$('input')
+	.on('mouseover',function(e){
+		$(this).removeClass('error');
+	});
+
+
